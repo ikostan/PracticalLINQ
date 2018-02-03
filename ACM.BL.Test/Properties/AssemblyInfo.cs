900a84f82fd4e6d9306a8a22b306199274f7be2f
ikostan <igorkostan@gmail.com>
2018-02-03 15:43:46 -0800

    Comments, LINQ (method, quaery)

--- ACM.BL.Test/Properties/AssemblyInfo.cs
+++ ACM.BL.Test/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PracticalLINQ")]
+[assembly: AssemblyTitle("ACM.BL.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PracticalLINQ")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("InStep Technologies, Inc.")]
+[assembly: AssemblyProduct("ACM.BL.Test")]
+[assembly: AssemblyCopyright("Copyright © InStep Technologies, Inc. 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("573ca2e5-ab1c-436f-a96f-03fcec5e1176")]
+[assembly: Guid("ca5b6734-1387-4ed2-a6b3-a848065531f2")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ ACM.BL/CustomerRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ACM.BL
+{
+    public class CustomerRepository
+    {
+        /// <summary>
+        /// Find a customer by id
+        /// </summary>
+        /// <param name="customerList"></param>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public Customer Find(List<Customer> customerList, int customerId)
+        {
+            Customer foundCustomer = null;
+
+            //Generic method, using loop in order to find the customer 
+            //foreach (var c in customerList)
+            //{
+            //    if (c.CustomerId == customerId)
+            //    {
+            //        foundCustomer = c;
+            //        break;
+            //    }
+            //}
+
+            //Using LINQ Quaery syntax:            
+            //var c = from cust in customerList
+            //        where cust.CustomerId == customerId
+            //        select cust;
+
+
+            //Using LINQ Method quaery:
+            var c = customerList.Where(
+                (customer) => customer.CustomerId == customerId);
+
+            foundCustomer = c.ToArray()[0];
+
+            return foundCustomer;
+        }
+
+        /// <summary>
+        /// Predefined list of customers.
+        /// </summary>
+        /// <returns></returns>
+        public List<Customer> Retrieve()
+        {
+            List<Customer> custList = new List<Customer>
+                    {new Customer() 
+                          { CustomerId = 1, 
+                            FirstName="Frodo",
+                            LastName = "Baggins",
+                            EmailAddress = "fb@hob.me",
+                            CustomerTypeId=1},
+                    new Customer() 
+                          { CustomerId = 2, 
+                            FirstName="Bilbo",
+                            LastName = "Baggins",
+                            EmailAddress = "bb@hob.me",
+                            CustomerTypeId=null},
+                    new Customer() 
+                          { CustomerId = 3, 
+                            FirstName="Samwise",
+                            LastName = "Gamgee",
+                            EmailAddress = "sg@hob.me",
+                            CustomerTypeId=1},
+                    new Customer() 
+                          { CustomerId = 4, 
+                            FirstName="Rosie",
+                            LastName = "Cotton",
+                            EmailAddress = "rc@hob.me",
+                            CustomerTypeId=2}};
+            return custList;
+        }
+    }
+}

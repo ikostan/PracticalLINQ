b5c8df36ea1476f235054239bd83d66616877dfe
ikostan <igorkostan@gmail.com>
2018-02-03 20:53:57 -0800

    CustomerTypeRepository added

---
+++ ACM.BL/CustomerTypeRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ACM.BL
+{
+    class CustomerTypeRepository
+    {
+        public List<CustomerType> Retrieve()
+        {
+            List<CustomerType> typeList = new List<CustomerType>
+            {
+                new CustomerType()
+                {
+                    CustomerTypeId = 1,
+                    TypeName = "Corporate",
+                    DisplayOrder = 2
+                },
+                 new CustomerType()
+                {
+                    CustomerTypeId = 2,
+                    TypeName = "Individual",
+                    DisplayOrder = 1
+                },
+                  new CustomerType()
+                {
+                    CustomerTypeId = 3,
+                    TypeName = "Educatore",
+                    DisplayOrder = 4
+                },
+                   new CustomerType()
+                {
+                    CustomerTypeId = 4,
+                    TypeName = "Goverment",
+                    DisplayOrder = 3
+                }
+            };
+
+            return typeList;
+        }
+    }
+}

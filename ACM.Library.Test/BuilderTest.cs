6c77939a032ac1896bea9d735fcd7b50f43ab6a2
ikostan <igorkostan@gmail.com>
2018-02-03 18:04:22 -0800

    BuildIntSequanceTest

--- ACM.Library.Test/BuilderTest.cs
+++ ACM.Library.Test/BuilderTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace ACM.Library.Test
 {
@@ -7,9 +9,19 @@ namespace ACM.Library.Test
     public class BuilderTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BuildIntSequanceTest()
         {
+            //Arrange
+            Builder b = new Builder();
 
+            //Act
+            var range = b.BuildIntSequance();
+
+            //Assert
+            Assert.IsNotNull(range);
+            Assert.AreEqual(10, range.Count());
+            Assert.AreEqual(0, range.First());
+            Assert.AreEqual(9, range.Last());
         }
     }
 }
--- ACM.Library/Builder.cs
+++ ACM.Library/Builder.cs
@@ -5,8 +5,15 @@ using System.Text;
 
 namespace ACM.Library
 {
-    class Builder
+    public class Builder
     {
-
+        /// <summary>
+        /// Build and return sequance of int numbers
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<int> BuildIntSequance()
+        {
+            return Enumerable.Range(0, 10);
+        }
     }
 }

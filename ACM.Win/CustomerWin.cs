e0329640c63ec18b755e083979532c0c308dc338
ikostan <igorkostan@gmail.com>
2018-02-05 17:52:12 -0800

    Reference to the business layer

--- ACM.Win/CustomerWin.cs
+++ ACM.Win/CustomerWin.cs
@@ -13,55 +13,9 @@ namespace ACM.Win
     public partial class CustomerWin : Form
     {
 
-        CustomerRepository customerRepository = new CustomerRepository();
-        
         public CustomerWin()
         {
             InitializeComponent();
-        }
-
-        public void CustomerWin_Load(object sender, EventArgs e)
-        {
-            var customerList = customerRepository.Retrieve();
-
-            CustomerComboBox.DisplayMember = "Name";
-            CustomerComboBox.ValueMember = "CustomerId";
-            CustomerComboBox.DataSource = customerRepository.GetNamesAndId(customerList);
-        }
-
-        private void GetCustomersButton_Click(object sender, EventArgs e)
-        {
-            //CustomerGridView.DataSource = customerRepository.Retrieve();
-
-            var customerList = customerRepository.Retrieve();
-            //CustomerGridView.DataSource = customerRepository.SortByName(customerList).ToList();
-
-            //CustomerGridView.DataSource = customerRepository.GetOverdueCustomers(customerList).ToList();
-
-            //var unpaidCustomerList = customerRepository.GetOverdueCustomers(customerList);
-            //CustomerGridView.DataSource = customerRepository.SortByName(unpaidCustomerList).ToList();
-
-            CustomerTypeRepository customerTypeRepository = new CustomerTypeRepository();
-            var customerTypeList = customerTypeRepository.Retrieve();
-
-            CustomerGridView.DataSource = customerRepository.GetNamesAndType(customerList,
-                                                customerTypeList);
-        }
-
-        private void CustomerComboBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (CustomerComboBox.SelectedValue != null)
-            {
-                int customerId;
-                if (int.TryParse(CustomerComboBox.SelectedValue.ToString(), out customerId))
-                {
-                    var customerList = customerRepository.Retrieve();
-
-                    CustomerGridView.DataSource = new List<Customer>() 
-                                {customerRepository.Find(customerList,
-                                                            customerId)};
-                }
-            }
-        }
+        }       
     }
 }

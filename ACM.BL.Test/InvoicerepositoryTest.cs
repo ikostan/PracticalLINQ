56e06a01cb689b3ca30dd1c5083f274cf3120ab2
ikostan <igorkostan@gmail.com>
2018-02-05 20:42:28 -0800

    InvoiceRepositoryTest

--- ACM.BL.Test/InvoicerepositoryTest.cs
+++ ACM.BL.Test/InvoicerepositoryTest.cs
@@ -14,82 +14,59 @@ namespace ACM.BL.Test
         [TestMethod]
         public void CalculateMeanTest()
         {
-            // Arrange
-            InvoiceRepository repository = new InvoiceRepository();
-            var invoiceList = repository.Retrieve();
+            // Arrange           
 
             // Act
-            var actual = repository.CalculateMean(invoiceList);
 
             // Assert
-            Assert.AreEqual(6.875M, actual);
         }
 
         [TestMethod]
         public void CalculateMedianTest()
         {
             // Arrange
-            InvoiceRepository repository = new InvoiceRepository();
-            var invoiceList = repository.Retrieve();
 
             // Act
-            var actual = repository.CalculateMedian(invoiceList);
 
             // Assert
-            Assert.AreEqual(10M, actual);
         }
 
         [TestMethod]
         public void CalculateModeTest()
         {
             // Arrange
-            InvoiceRepository repository = new InvoiceRepository();
-            var invoiceList = repository.Retrieve();
 
             // Act
-            var actual = repository.CalculateMode(invoiceList);
 
             // Assert
-            Assert.AreEqual(10M, actual);
         }
 
         [TestMethod]
         public void CalculateTotalAmountInvoicedTest()
         {
             // Arrange
-            InvoiceRepository repository = new InvoiceRepository();
-            var invoiceList = repository.Retrieve();
 
             // Act
-            var actual = repository.CalculateTotalAmountInvoiced(invoiceList);
 
             // Assert
-            Assert.AreEqual(1333.14M, actual);
         }
 
         [TestMethod]
         public void CalculateTotalUnitsSoldTest()
         {
             // Arrange
-            InvoiceRepository repository = new InvoiceRepository();
-            var invoiceList = repository.Retrieve();
 
             // Act
-            var actual = repository.CalculateTotalUnitsSold(invoiceList);
 
             // Assert
-            Assert.AreEqual(136, actual);
         }
 
         [TestMethod]
         public void GetInvoiceTotalByIsPaidTest()
         {
             // Arrange
-            InvoiceRepository repository = new InvoiceRepository();
-            var invoiceList = repository.Retrieve();
 
             // Act
-            var query = repository.GetInvoiceTotalByIsPaid(invoiceList);
 
             // NOT REALLY A TEST
         }
@@ -98,11 +75,8 @@ namespace ACM.BL.Test
         public void GetInvoiceTotalByIsPaidAndMonthTest()
         {
             // Arrange
-            InvoiceRepository repository = new InvoiceRepository();
-            var invoiceList = repository.Retrieve();
 
             // Act
-            var query = repository.GetInvoiceTotalByIsPaidAndMonth(invoiceList);
 
             // NOT REALLY A TEST
         }

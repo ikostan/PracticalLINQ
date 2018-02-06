8645d7ad4ef1d9e9a630ff05f8d7ccbdd3531713
ikostan <igorkostan@gmail.com>
2018-02-05 20:27:53 -0800

    Invoice class refactoring

--- ACM.BL/Invoice.cs
+++ ACM.BL/Invoice.cs
@@ -7,12 +7,20 @@ namespace ACM.BL
 {
     public class Invoice
     {
+        //
         public int InvoiceId { get; set; }
         public int CustomerId { get; set; }
         public DateTime InvoiceDate { get; set; }
         public DateTime DueDate { get; set; }
         public bool? IsPaid { get; set; }
 
+        //
+        public decimal Amount { get; set; }
+        public int NumberOfUnits { get; set; }
+        public decimal DiscountPercent { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        //
         public override string ToString()
         {
             return String.Format("Invoice id: {0}, Customer id: {1}", InvoiceId, CustomerId);

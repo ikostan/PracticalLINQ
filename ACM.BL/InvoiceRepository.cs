fc3e2ac1958d7dc879855e4ab6a674f8e2dcf14e
ikostan <igorkostan@gmail.com>
2018-02-05 20:30:52 -0800

    InvoiceRepository refactoring

--- ACM.BL/InvoiceRepository.cs
+++ ACM.BL/InvoiceRepository.cs
@@ -21,28 +21,40 @@ namespace ACM.BL
                     CustomerId = 1,
                     InvoiceDate = new DateTime(2013, 6, 20),
                     DueDate = new DateTime(2013, 8, 29),
-                    IsPaid = null
+                    IsPaid = null,
+                    Amount = 199.99M,
+                    NumberOfUnits=20,
+                    DiscountPercent=0M
                 },
                 new Invoice(){
                     InvoiceId = 2,
                     CustomerId =1,
                     InvoiceDate = new DateTime(2013, 7, 20),
                     DueDate = new DateTime(2013, 8, 20),
-                    IsPaid = null
+                    IsPaid = null,
+                    Amount = 98.50M,
+                    NumberOfUnits=10,
+                    DiscountPercent=10M
                 },
                 new Invoice(){
                     InvoiceId = 3,
                     CustomerId = 2,
                     InvoiceDate = new DateTime(2013, 7, 25),
                     DueDate = new DateTime(2013, 8, 25),
-                    IsPaid = null
+                    IsPaid = null,
+                    Amount = 500M,
+                    NumberOfUnits=42,
+                    DiscountPercent=10M
                 },
                 new Invoice(){
                     InvoiceId = 4,
                     CustomerId = 3,
                     InvoiceDate = new DateTime(2013, 7, 1),
                     DueDate = new DateTime(2013, 9, 1),
-                    IsPaid = null
+                    IsPaid = null,
+                    Amount = 75M,
+                    NumberOfUnits=7,
+                    DiscountPercent=0M
                 }
             };
 
